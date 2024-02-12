5b932b73091f9b55da98578a0dbeef4ffc99bc93
Carlos Henrique <ichfs9108@gmail.com>
2024-02-12 15:47:41 -0300

    Project Created

---
+++ musics/musics/src/test/java/com/musics/MusicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
