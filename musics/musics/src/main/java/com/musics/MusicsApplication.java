a863355a7bb6733462a3fabaaf0ab69892ef4b43
Carlos Henrique <ichfs9108@gmail.com>
2024-02-12 17:56:54 -0300

    Configurando as bases do projeto

--- musics/musics/src/main/java/com/musics/MusicsApplication.java
+++ musics/musics/src/main/java/com/musics/MusicsApplication.java
@@ -1,13 +1,20 @@
 package com.musics;
 
+import com.musics.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusicsApplication {
+public class MusicsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicsApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.exibeMenu();
+    }
 }
